751f0ab02f49a6d05145361f22b1f2ff1549333b
Your Name <you@example.com>
2020-01-29 11:20:31 +0530

    Updates

---
+++ precache-manifest.af0aab450028b6e841bdaf2417a29f52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d38bfa65097173883409f0a789dc2ff",
+    "url": "/mahanor123/Recat-recipe-finder/index.html"
+  },
+  {
+    "revision": "8163b283987bc1075e82",
+    "url": "/mahanor123/Recat-recipe-finder/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d4e8a681b57b093d12ba",
+    "url": "/mahanor123/Recat-recipe-finder/static/css/main.33920ca6.chunk.css"
+  },
+  {
+    "revision": "8163b283987bc1075e82",
+    "url": "/mahanor123/Recat-recipe-finder/static/js/2.eae8b42c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mahanor123/Recat-recipe-finder/static/js/2.eae8b42c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4e8a681b57b093d12ba",
+    "url": "/mahanor123/Recat-recipe-finder/static/js/main.930e1818.chunk.js"
+  },
+  {
+    "revision": "6b9f55e30d03b94ff4d3",
+    "url": "/mahanor123/Recat-recipe-finder/static/js/runtime-main.48bd8c3d.js"
+  }
+]);
